52545a241e1e2328fb50a3a0f34b9f91c016294f
kmerten10 <kelsey.merten@gmail.com>
2023-11-13 12:21:59 -0600

    updated export

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -9,7 +9,7 @@ const apiUrl = 'https://my-flix-app-66e818e7b7de.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
-export class UserRegistrationService {
+export class FetchApiDataService {
   // Inject the HttpClient module to the constructor params
   // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
